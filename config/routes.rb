642972137e3b1c63024a13eaaddbdf664c4f6c3f
Matt Kellough <mrk10a@gmail.com>
2016-04-05 14:01:08 -0400

    modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  get 'cart/index'
+
+  get '/cart' => 'cart#index'
+  get '/cart/clear' => 'cart#clearCart'
+  get '/cart/:id' => 'cart#add'
 
   resources :products
   get 'page/home'
